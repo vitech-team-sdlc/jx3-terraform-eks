483c4de22769bf79d04c77b93822e7c316324235
Andriy Pentsak <andriy.pentsak@vitechteam.com>
2021-12-14 10:59:44 +0200

    added github actions

--- main.tf
+++ main.tf
@@ -1,37 +1,70 @@
 provider "aws" {
-  region  = var.region
-  profile = var.profile
+  region  = "us-east-2"
 }
 
 module "eks-jx" {
-  source          = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.15.12"
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  region          = var.region
-  vault_user      = var.vault_user
-  is_jx2          = false
-  jx_git_url      = var.jx_git_url
-  jx_bot_username = var.jx_bot_username
-  jx_bot_token    = var.jx_bot_token
-  force_destroy   = var.force_destroy
+  source              = "github.com/vitech-team-sdlc/terraform-aws-eks-jx?ref=v1.18.6-rc-1"
+  cluster_name        = "apentsak"
+  cluster_version     = "1.20"
+  region              = "us-east-2"
+  vault_user          = ""
+  is_jx2              = false
+  jx_git_url          = "https://github.com/AndrewsBot/dev-env.git"
+  jx_bot_username     = "AndrewsBot"
+  jx_bot_token        = var.jx_bot_token
+  force_destroy       = true
+  nginx_chart_version = "3.34.0"
 
-  volume_size = var.volume_size
+  enable_spot_instances                = true
+  enable_worker_groups_launch_template = true
 
-  apex_domain                    = var.apex_domain
-  subdomain                      = var.subdomain
-  create_and_configure_subdomain = var.create_and_configure_subdomain
-  enable_external_dns            = var.enable_external_dns
+  enable_tls                     = true
+  force_destroy_subdomain        = true
+  create_nginx                   = true
+  apex_domain                    = "sdlcvitech.com"
+  subdomain                      = "apentsak"
+  create_and_configure_subdomain = true
+  production_letsencrypt         = false
 
-  production_letsencrypt = var.lets_encrypt_production
-  node_machine_type      = var.node_machine_type
-  tls_email              = var.tls_email
-  enable_tls             = var.enable_tls
-  min_node_count         = var.min_node_count
-  max_node_count         = var.max_node_count
-  desired_node_count     = var.desired_node_count
+  workers = {
 
-  nginx_chart_version  = var.nginx_chart_version
-  install_kuberhealthy = var.install_kuberhealthy
+    main = {
+      k8s_labels              = ""
+      k8s_taints              = ""
+      on_demand_base_capacity = 1
+      asg_min_size            = 1
+      asg_max_size            = 3
+      asg_desired_capacity    = 1
+      root_volume_size        = 60
+      root_encrypted          = true
+      tags                    = [
+      ]
+      override_instance_types = [
+        "m5.large"
+      ]
+    }
+
+    pipelines = {
+      k8s_labels              = "pipeline=true"
+      k8s_taints              = "pipelines=true:PreferNoSchedule"
+      on_demand_base_capacity = 0
+      asg_min_size            = 1
+      asg_max_size            = 1
+      asg_desired_capacity    = 1
+      root_volume_size        = 10
+      root_encrypted          = true
+      tags                    = [
+        {
+          key                 = "pipeline"
+          propagate_at_launch = "true"
+          value               = "true"
+        }
+      ]
+      override_instance_types = [
+        "m5.xlarge", "m5a.xlarge", "m5d.xlarge", "m5ad.xlarge", "m5n.xlarge", "m5.2xlarge", "m5a.2xlarge", "m5d.2xlarge", "m5ad.2xlarge", "m5n.2xlarge"
+      ]
+    }
+  }
 }
 
 
