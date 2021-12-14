1a54e03331c8649de2a2efecb65be3f09309dc4d
AndrewsBot <85612339+AndrewsBot@users.noreply.github.com>
2021-12-14 09:49:01 +0200

    Update from Jenkins x master (#1)
    * fix: add missing variables, and update module version to latest
    
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>
    
    * chore: upgrade version stream
    
    * docs: Added AWSCLI v2 installation instructions for AWS SSO
    
    Signed-off-by: Don Reid <don.reid@inclus.com>
    
    * refactor: support eks 1.21
    
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>
    
    * chore: upgrade version stream
    
    * chore: manually remove old ref until the updatebot rule that created it is disabled
    
    * chore: upgrade version stream
    
    * removed ref on non registry URL
    
    * upgrade to latest eks-jx version
    
    * ran terraform init --upgrade
    
    * chore: upgrade version stream
    
    * fix: Remove version details 
    
    Version can't be part of the source field
    
    * chore: update tf module version
    
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>
    
    * chore: upgrade version stream
    
    * fix: change the source string format
    
    Signed-off-by: ankitm123 <ankitmohapatra123@gmail.com>
    
    * chore: upgrade version stream
    
    * update eks-jx
    
    Co-authored-by: ankitm123 <ankitmohapatra123@gmail.com>
    Co-authored-by: James Strachan <james.strachan@gmail.com>
    Co-authored-by: jenkins-x-bot <jenkins-x@googlegroups.com>
    Co-authored-by: Don Reid <86226009+ReidDon@users.noreply.github.com>
    Co-authored-by: James Rawlings <rawlingsj80@gmail.com>
    Co-authored-by: Marc Smith <mr.marcsmith@me.com>
    Co-authored-by: Daniel <72084247+danielosg@users.noreply.github.com>
    Co-authored-by: Andriy Pentsak <partner.andriy.pentsak@philips.com>

--- main.tf
+++ main.tf
@@ -1,29 +1,37 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
 module "eks-jx" {
-  source = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.15.12"
-  cluster_name = var.cluster_name
+  source          = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.15.12"
+  cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  region = var.region
-  vault_user = var.vault_user
-  is_jx2 = false
-  jx_git_url = var.jx_git_url
+  region          = var.region
+  vault_user      = var.vault_user
+  is_jx2          = false
+  jx_git_url      = var.jx_git_url
   jx_bot_username = var.jx_bot_username
-  jx_bot_token = var.jx_bot_token
-  force_destroy = var.force_destroy
+  jx_bot_token    = var.jx_bot_token
+  force_destroy   = var.force_destroy
 
   volume_size = var.volume_size
 
-  apex_domain = var.apex_domain
-  subdomain = var.subdomain
+  apex_domain                    = var.apex_domain
+  subdomain                      = var.subdomain
   create_and_configure_subdomain = var.create_and_configure_subdomain
-  enable_external_dns = var.enable_external_dns
+  enable_external_dns            = var.enable_external_dns
 
   production_letsencrypt = var.lets_encrypt_production
-  node_machine_type = var.node_machine_type
-  tls_email = var.tls_email
-  enable_tls = var.enable_tls
-  min_node_count = var.min_node_count
-  max_node_count = var.max_node_count
-  desired_node_count = var.desired_node_count
+  node_machine_type      = var.node_machine_type
+  tls_email              = var.tls_email
+  enable_tls             = var.enable_tls
+  min_node_count         = var.min_node_count
+  max_node_count         = var.max_node_count
+  desired_node_count     = var.desired_node_count
+
+  nginx_chart_version  = var.nginx_chart_version
+  install_kuberhealthy = var.install_kuberhealthy
 }
 
 
