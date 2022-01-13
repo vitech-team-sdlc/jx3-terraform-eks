86dc8be8b25e4b3865ba84ad6095cb5658d7316f
AndrewsBot <85612339+AndrewsBot@users.noreply.github.com>
2022-01-13 15:31:45 +0200

    Added github actions (#2)
    * added github actions
    
    * fix: terraform apply
    
    * Update github action according to comments
    
    * fix: update variables
    
    * chore: change format to json for variables
    
    Co-authored-by: serhiykrupka <serhiy.krupka@vitech.com.ua>

--- main.tf
+++ main.tf
@@ -1,39 +1,42 @@
 provider "aws" {
-  region  = var.region
-  profile = var.profile
+  region = var.region
 }
 
 module "eks-jx" {
-  source          = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.15.12"
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  region          = var.region
-  vault_user      = var.vault_user
-  is_jx2          = false
-  jx_git_url      = var.jx_git_url
-  jx_bot_username = var.jx_bot_username
-  jx_bot_token    = var.jx_bot_token
-  force_destroy   = var.force_destroy
+  source                  = "github.com/vitech-team-sdlc/terraform-aws-eks-jx?ref=v1.18.6-rc-5"
+  cluster_name            = var.cluster_name
+  cluster_version         = var.cluster_version
+  region                  = var.region
+  vault_user              = var.vault_user
+  is_jx2                  = false
+  jx_git_url              = var.jx_git_url
+  jx_bot_username         = var.jx_bot_username
+  jx_bot_token            = var.jx_bot_token
+  force_destroy           = var.force_destroy
+  force_destroy_subdomain = var.force_destroy_subdomain
 
-  volume_size = var.volume_size
-
+  create_nginx                   = true
   apex_domain                    = var.apex_domain
   subdomain                      = var.subdomain
   create_and_configure_subdomain = var.create_and_configure_subdomain
   enable_external_dns            = var.enable_external_dns
 
   production_letsencrypt = var.lets_encrypt_production
-  node_machine_type      = var.node_machine_type
   tls_email              = var.tls_email
   enable_tls             = var.enable_tls
-  min_node_count         = var.min_node_count
-  max_node_count         = var.max_node_count
-  desired_node_count     = var.desired_node_count
 
-  nginx_chart_version  = var.nginx_chart_version
-  install_kuberhealthy = var.install_kuberhealthy
+  hosted_access_key                     = var.hosted_access_key
+  hosted_secret_key                     = var.hosted_secret_key
+  domain_registered_in_same_aws_account = var.domain_registered_in_same_aws_account
+
+  enable_k8s_deployment_cluster_autoscaler = var.enable_k8s_deployment_cluster_autoscaler
+
+  nginx_chart_version                  = var.nginx_chart_version
+  install_kuberhealthy                 = var.install_kuberhealthy
+  enable_spot_instances                = var.enable_spot_instances
+  enable_worker_groups_launch_template = var.enable_worker_groups_launch_template
+  workers                              = var.workers
 }
-
 
 // IF YOU NEED ADD access to docker repository from external AWS account/EKS (multi-cluster setup)
 //provider "aws" {
